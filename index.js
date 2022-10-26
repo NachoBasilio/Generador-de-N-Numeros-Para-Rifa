2c14216a07e1b5973b8df862db2cbaadc53cce89
IgnaDev <arcortoon@gmail.com>
2022-10-26 17:38:43 -0300

    Ya se puede cambiar de color

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const sumar = document.getElementById("sumar")
 const resta = document.getElementById("resta")
+const rosa = document.getElementById("rosa")
+const amarillo = document.getElementById("amarillo")
+const azul = document.getElementById("azul")
+const canvas = document.getElementById("canvas")
+
 
 const cambiaValores =(operacion)=>{
     const nodos=document.getElementsByClassName("numero")
@@ -22,4 +27,13 @@ sumar.addEventListener("click", ()=>{
 })
 resta.addEventListener("click", ()=>{
     cambiaValores() 
+})
+rosa.addEventListener("click", ()=>{
+    canvas.style.backgroundImage = "url('./3.png')";
+})
+amarillo.addEventListener("click", ()=>{
+    canvas.style.backgroundImage = "url('./1.png')";
+})
+azul.addEventListener("click", ()=>{
+    canvas.style.backgroundImage = "url('./2.png')";
 })
